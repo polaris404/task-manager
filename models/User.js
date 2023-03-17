dd08c335f90c2b4c0e60a408e93c72e12e956d92
spicymaterial <spicy_material@outlook.com>
2023-03-17 12:56:14 +0530

    Updated Models

--- models/User.js
+++ models/User.js
@@ -4,69 +4,68 @@ const jwt = require("jsonwebtoken");
 const validator = require("validator");
 
 require("dotenv").config;
-const TaskSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: [true, "must provide name"],
-		trim: true,
-		maxlength: [20, "name can not be more than 20 characters"],
+const TaskSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "must provide name"],
+			trim: true,
+			maxlength: [20, "name can not be more than 20 characters"],
+		},
+		completed: {
+			type: Boolean,
+			default: false,
+		},
+		createdAt: Date,
+		updatedAt: Date,
+		completedAt: Date,
 	},
-	completed: {
-		type: Boolean,
-		default: false,
-	},
-	createdAt: Date,
-	updatedAt: Date,
-	completedAt: Date,
-});
+	{ timestamps: true }
+);
 
-const UserSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: [true, "Must provide name"],
-		trim: true,
-		maxlength: [20, "Name cannot be more than 20 characters"],
-	},
-	email: {
-		type: String,
-		unique: true,
-		required: [true, "Must provide email"],
-		validator: {
-			validator: validator.isEmail,
-			message: "Please provide valid email",
+const UserSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "Must provide name"],
+			trim: true,
+			maxlength: [20, "Name cannot be more than 20 characters"],
+		},
+		email: {
+			type: String,
+			unique: true,
+			required: [true, "Must provide email"],
+			validator: {
+				validator: validator.isEmail,
+				message: "Please provide valid email",
+			},
+		},
+		pass: {
+			type: String,
+			required: [true, "Must provide password"],
+		},
+		verificationToken: String,
+		isVerified: {
+			type: Boolean,
+			default: false,
+		},
+		verifiedAt: Date,
+		tasks: {
+			type: [TaskSchema],
 		},
 	},
-	pass: {
-		type: String,
-		required: [true, "Must provide password"],
-	},
-	verificationToken: String,
-	isVerified: {
-		type: Boolean,
-		default: false,
-	},
-	verifiedAt: Date,
-	tasks: {
-		type: [TaskSchema],
-	},
-});
+	{ timestamps: true }
+);
 UserSchema.index({ email: 1 }, { unique: true });
 
-UserSchema.pre("save", async function (next) {
-	const salt = await bcrypt.genSalt(9);
-	this.pass = await bcrypt.hash(this.pass, salt);
-	next();
-});
-
-UserSchema.methods.createJWT = function () {
-	return jwt.sign(
-		{ userID: this._id, name: this.name },
-		process.env.JWT_SECRET,
-		{
-			expiresIn: process.env.JWT_LIFETIME,
-		}
-	);
-};
+// UserSchema.pre("save", async function (next) {
+// 	const salt = await bcrypt.genSalt(9);
+// 	const pass = await bcrypt.hash(this.pass, salt);
+// 	console.log(`Initial hash: ${pass}`);
+// this.pass = await bcrypt.hash(this.pass, salt);
+// this.pass = pass;
+// 	next();
+// });
 
 UserSchema.methods.comparePass = async function (candidatePass) {
 	const isMatch = await bcrypt.compare(candidatePass, this.pass);
